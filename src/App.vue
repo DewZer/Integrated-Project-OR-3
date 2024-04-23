14c4a9ac4a83879639823122587167a88acbd990
Teamsyy <meowmeow2346@gmail.com>
2024-04-23 15:46:43 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <h1 class="text-3xl font-bold underline">
+    Hello Frontend
+  </h1>
+
+  </header>
+</template>
+
+<style scoped>
+
+</style>
