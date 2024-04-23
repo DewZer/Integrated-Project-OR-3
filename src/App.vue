e407c2e681371f80bb063c76b096a968d232861f
Teamsyy <93755080+Teamsyy@users.noreply.github.com>
2024-04-23 16:13:02 +0700

    Add Table

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
-<script setup>
-
-</script>
-
 <template>
-  <header>
-    <h1 class="text-3xl font-bold underline">
-    Hello Frontend
-  </h1>
-
-  </header>
+  <div class="flex justify-center">IT Bangmod Kradan Kanban</div>
+  <br>
+  <div class="w-screen flex items-center justify-center overflow-x-hidden">
+    <table class="min-w-full w-full">
+      <thead>
+        <tr class="bg-gray-200 w-full">
+          <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Title</th>
+          <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Assignees</th>
+          <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
+        </tr>
+      </thead>
+      <!-- <tbody class="bg-white divide-y divide-gray-200">
+        <tr v-for="(todo, index) in todos" :key="index">
+          <td class="px-6 py-4 whitespace-nowrap">{{ todo.title }}</td>
+          <td class="px-6 py-4 whitespace-nowrap">{{ todo.assignees.join(', ') }}</td>
+          <td class="px-6 py-4 whitespace-nowrap">
+            <span :class="statusClasses(todo.status)">{{ todo.status }}</span>
+          </td>
+        </tr>
+      </tbody> -->
+    </table>
+  </div>
 </template>
 
 <style scoped>
