5ebd3e4e732f2385da78ab5ecf06c4597c0759fe
Teamsyy <93755080+Teamsyy@users.noreply.github.com>
2024-04-26 01:03:58 +0700

    add cypress and update ui

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,6 @@
 <script setup>
 import { ref, onMounted } from 'vue';
-const tableHeader = "px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider";
-const todos = ref([]);
-const showModal = ref(false);
-const selectedTodo = ref(null);
 
-const editTodo = (todo) => {
-  selectedTodo.value = todo;
-  showModal.value = true;
-};
-
-console.log(todos.value);
 
 const fetchData = async () => {
   try {
@@ -23,64 +13,113 @@ const fetchData = async () => {
 };
 
 onMounted(fetchData);
+const tableHeader = "px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider";
+const todos = ref([]);
+const showModal = ref(false);
+const selectedTodo = ref(null);
+
+
+const editTodo = (todo) => {
+  selectedTodo.value = todo;
+  showModal.value = true;
+};
+
+console.log(todos.value);
+
+const formatStatus = (status) => {
+  switch (status) {
+    case 'NO_STATUS':
+      return 'No Status';
+    case 'TODO':
+      return 'To Do';
+    case 'DOING':
+      return 'Doing';
+    case 'DONE':
+      return 'Done';
+    default:
+      return status;
+  }
+};
+
+const formatDate = (date) => {
+  const dt = new Date(date);
+  const day = String(dt.getDate()).padStart(2, '0');
+  const month = String(dt.getMonth() + 1).padStart(2, '0'); // January is 0!
+  const year = dt.getFullYear();
+  const hours = String(dt.getHours()).padStart(2, '0');
+  const minutes = String(dt.getMinutes()).padStart(2, '0');
+  const seconds = String(dt.getSeconds()).padStart(2, '0');
+
+  return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+};
 </script>
 
 <template>
-  <div class="overflow-x-auto flex justify-center items-center w-full h-screen">
-  <table class="table w-full h-full border-4">
-    <!-- head -->
-    <thead class="h-20 justify-center items-center">
-      <tr class="">
-        <th class="text-2xl">Title</th>
-        <th class="text-2xl">Assignees</th>
-        <th class="text-2xl">Status</th>
-        <th class="text-2xl"></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="(todo, index) in todos" :key="index" 
-      class="itbkk-items hover h-12">
-        <td class="itbkk-title text-lg">{{ todo.title }}</td>
-        <td class="itbkk-assignees text-lg">{{ todo.assignees }}</td>
-        <td class="itbkk-status text-lg">{{ todo.status }}</td>
+  <div class="flex min-w-full min-h-screen m-0 p-0">
+    <table v-if="todos.length"
+      class="table-auto w-full h-full border-4 bg-white shadow-md rounded-lg overflow-hidden mx-auto">
+      <!-- head -->
+      <thead class="bg-blue-500 text-white">
+        <tr>
+          <th class="px-4 py-2 text-xl">Title</th>
+          <th class="px-4 py-2 text-xl">Assignees</th>
+          <th class="px-4 py-2 text-xl w-1/3">Status</th>
+          <th class="px-4 py-2 text-xl"></th>
+        </tr>
+      </thead>
+      <!-- body -->
+      <tbody>
+        <tr v-for="(todo, index) in todos" :key="index" class="itbkk-item hover:bg-gray-100 h-12">
+          <td class="border px-4 py-2 itbkk-title text-lg">{{ todo.title }}</td>
+          <td class="border px-4 py-2 itbkk-assignees text-lg italic">{{ todo.assignees ? todo.assignees : 'Unassigned'
+            }}</td>
+          <td class="border px-4 py-2 itbkk-status text-lg w-1/3">{{ formatStatus(todo.status) }}</td>
+          <td class="border px-4 py-2"><button @click="editTodo(todo)"
+              class="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Edit</button></td>
+        </tr>
+      </tbody>
+    </table>
 
-        <td><button @click="editTodo(todo)" class="btn">Edit</button></td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-
-
-<dialog v-if="showModal" id="my_modal_2" class="modal bg-gray-500 bg-opacity-50" open>
-  <div class="modal-box w-3/4 h-3/4 p-6">
-    <h2 class="text-2xl font-bold mb-4 text-center">{{ selectedTodo.title }}</h2>
-    <div class="p-4 mt-2 border rounded bg-white">
-      <p class="text-lg text-gray-700 font-semibold">
-        Description:
-        <br> {{ selectedTodo.description.split(' ').slice(0, 100).join(' ') }}...
-      </p>
-    </div>
-    <p class="text-md text-white-700 mt-4">
-      Assignees: <span class="font-semibold">{{ selectedTodo.assignees }}</span>
-    </p>
-    <p class="text-md text-white-700mt-2">
-      Status: <span class="font-semibold">{{ selectedTodo.status }}</span>
-    </p>
-    <p class="text-md text-white-700mt-2">
-      Created On: <span class="font-semibold">{{ selectedTodo.createdOn }}</span>
-    </p>
-    <p class="text-md text-white-700 mt-2">
-      Updated On: <span class="font-semibold">{{ selectedTodo.updatedOn }}</span>
-    </p>
-    <div class="modal-action mt-6 flex justify-end">
-      <button class="btn bg-blue-500 text-white mr-2" @click="showModal = false">Save</button>
-      <button class="btn bg-red-500 text-white" @click="showModal = false">Close</button>
+    <div v-else class="flex justify-center items-center w-full h-full">
+      <h2 class="text-2xl">No tasks available</h2>
     </div>
   </div>
-</dialog>
+
+  <!-- modal  -->
+  <dialog v-if="showModal" id="my_modal_2" class="modal bg-gray-500 bg-opacity-50" open>
+    <div class="modal-box w-3/4 h-3/4 p-6">
+      <h2 class="itbkk-title text-2xl font-bold mb-4 text-center overflow-wrap break-word word-break break-all">
+        {{ selectedTodo.title }}
+      </h2>
+
+      <div class="p-4 mt-2 border rounded bg-white overflow-auto h-2/4">
+        <p class="itbkk-description text-lg text-gray-700 font-semibold overflow-wrap break-word word-break break-all">
+          Description:
+          <br> {{ selectedTodo.description }}...
+        </p>
+      </div>
+
+      <p class="itbkk-assignees text-md text-white-700 mt-4">
+        Assignees: <span class="font-semibold">{{ selectedTodo.assignees ? selectedTodo.assignees : 'Unassigned'
+            }}</span>
+      </p>
+      <p class="itbkk-status text-md text-white-700mt-2">
+        Status: <span class="font-semibold">{{ formatStatus(selectedTodo.status) }}</span>
+      </p>
+      <p class="itbkk-created-on text-md text-white-700mt-2">
+        Created On: <span class="font-semibold">{{ formatDate(selectedTodo.createdOn) }}</span>
+      </p>
+      <p class="itbkk-updated-on text-md text-white-700 mt-2">
+        Updated On: <span class="font-semibold">{{ formatDate(selectedTodo.updatedOn) }}</span>
+      </p>
+      <div class="modal-action mt-6 flex justify-end">
+        <button class="btn bg-blue-500 text-white mr-2" @click="showModal = false">Save</button>
+        <button class="btn bg-red-500 text-white" @click="showModal = false">Close</button>
+      </div>
+    </div>
+  </dialog>
 
 </template>
 
 
-<style scoped>
-</style>
+<style scoped></style>
